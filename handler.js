c3d8af5426842c20b34582b756bb785feb2b60be
Saxena <ssaxena@bmc.com>
2023-12-13 12:05:50 +0530

    changes for handler

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Adding it to test the application',
         input: event,
       },
       null,
